37a74da0d1c00b97fd2be32f2e0c26e695e04326
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-05-04 09:54:33 +0100

    added rollup for images

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import sass from 'rollup-plugin-sass';
 import pkg from './package.json';
 import babel from 'rollup-plugin-babel';
+import image from 'rollup-plugin-image';
 
 export default {
     input: 'src/index.js',
@@ -19,7 +20,8 @@ export default {
             babel({
                 exclude: 'node_modules/**',
                 plugins: [ 'external-helpers' ]
-            })
+            }),
+            image()
         ],
     external: ['react', 'react-dom']
 }
