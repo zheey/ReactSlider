00e5e0e09ef53913a25a92afdd56bbfa37f6a8b6
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-05-06 09:50:28 +0100

    modified slider function to use setInterval

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import SliderIndicator from "./SliderIndicator";
 import sample1 from "./images/Group 69.png";
 import sample2 from "./images/image (5).png";
 import sample3 from "./images/image (6).png";
 
-let sliderTimeout;
 const sampleData = [sample1, sample2, sample3];
 
 const App = ({ sliderData=sampleData }) => {
     const [sliderIndex, setSliderIndex] = useState(0);
-    const setSlideIndexAuto = useCallback(() => {
-        if(sliderIndex < sliderData.length-1) {
-            setSliderIndex((sliderIndex + 1))
-        }else {
-            setSliderIndex(0);
-        }
-
-    }, []);
 
     useEffect( () => {
-        if(sliderTimeout){
-            clearInterval(sliderTimeout);
-        }
+        const interval = setInterval(() => {
+            if (sliderIndex < sliderData.length - 1) {
+                setSliderIndex((sliderIndex + 1))
+            } else {
+                return setSliderIndex((0));
+            }
 
-        sliderTimeout = setInterval(() => {
-            sliderTimeout = null;
-            setSlideIndexAuto();
         }, 3000);
+        return () => clearInterval(interval);
+    }, [sliderIndex]);
 
-    }, [setSlideIndexAuto]);
     return(
         <div className="slider-wrapper">
             <div className="slider-indicator-wrapper">
